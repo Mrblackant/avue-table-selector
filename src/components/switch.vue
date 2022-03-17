99b7af2e444185196696d46a8568d6c8dc0ef4f5
Mrblackant <anyapeng@bobfintech.com.cn>
2022-03-17 15:34:37 +0800

    修订依赖

---
+++ src/components/switch.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="handleClick">
+      按钮q
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'basicSwitch',
+  methods: {
+    handleClick() {
+      alert(1)
+    }
+  }
+}
+</script>
+
---
+++ src/index.js
@@ -0,0 +1,23 @@
+// import _Vue from "vue";
+// import basicSwitch from "./components/switch.vue"; // 引入封装好的组件
+// const coms = [basicSwitch]; // 将来如果有其它组件,都可以写到这个数组里
+
+// // 批量组件注册
+// const install = function (Vue) {
+//   if (!Vue) {
+//     window.Vue = Vue = _Vue;
+//   }
+//   coms.forEach((com) => {
+//     Vue.component(com.name, com);
+//   });
+// };
+
+// export default install; // 这个方法以后再使用的时候可以被use调用
+
+import basicSwitch from "./components/switch.vue"; // 引入封装好的组件
+
+export default {
+  install (Vue, options = {}) {
+    Vue.component('basic-switch', basicSwitch);
+  }
+};
